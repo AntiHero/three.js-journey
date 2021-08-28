7d98cf1974f5ffedc668da0139b7b7d9e88ff232
Aleksandr Schemelev <prophreaker@gmail.com>
2021-08-28 22:27:22 +0300

    feat: add gsap and basic animation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 const scene = new THREE.Scene();
 const canvas = document.querySelector('.webgl');
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-  color: 'blue',
+  color: 'lightgreen',
 });
 const mesh = new THREE.Mesh(geometry, material);
 mesh.position.x = .7;
@@ -27,8 +28,9 @@ camera.position.y = 1;
 camera.position.x = 1;
 scene.add(camera);
 
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+// Axis
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 
 camera.lookAt(mesh.position)
 
@@ -41,4 +43,23 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height);
 
-renderer.render(scene, camera);
+const clock = new THREE.Clock();
+
+gsap.to(mesh.position, {
+  duration: 1,
+  delay: 1.5,
+  x: -1,
+});
+
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  mesh.rotation.y = Math.sin(elapsedTime * Math.PI * 2);
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
