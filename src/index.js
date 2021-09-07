be1afd903f0563a72c859c1cb58c6b03d3700a06
Aleksandr Schemelev <prophreaker@gmail.com>
2021-09-07 23:54:35 +0300

    restore styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// import styles from "./css/style.css";
+import styles from "./css/style.css";
 // import gsap from "gsap";
 
 /**
