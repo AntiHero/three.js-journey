e271f43b3542ec11d3e485130e8bb73a39b0d4b3
Aleksandr Schemelev <prophreaker@gmail.com>
2021-10-07 16:20:56 +0300

    feat: add point light

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { DirectionalLightHelper } from 'three';
 /**
  * Base
  */
@@ -16,32 +15,67 @@ const scene = new THREE.Scene();
  * Lights
  */
 const ambientLight = new THREE.AmbientLight();
+ambientLight.intensity = 0.4;
 ambientLight.color = new THREE.Color(0xffffff);
 scene.add(ambientLight);
 
 // gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
 directionalLight.position.setX(1.5);
+
 directionalLight.castShadow = true;
+directionalLight.shadow.camera.right = 3;
+directionalLight.shadow.camera.left = -3;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.bottom = -2;
 directionalLight.shadow.mapSize.width = 1024;
 directionalLight.shadow.mapSize.height = 1024;
 directionalLight.shadow.camera.near = 1;
-directionalLight.shadow.camera.far = 6;
+directionalLight.shadow.camera.far = 4.5;
+directionalLight.shadow.radius = 10;
 
 const directionalCameraHelper = new THREE.CameraHelper(
   directionalLight.shadow.camera
 );
-scene.add(directionalLight);
-scene.add(directionalCameraHelper);
 
+// directionalLight.visible = false;
+scene.add(directionalLight);
+// scene.add(directionalCameraHelper);
+
+const spotLight = new THREE.SpotLight(0xffffff, 0.4, 10);
+
+spotLight.castShadow = true;
+spotLight.position.set(0, 2, 2);
+spotLight.shadow.camera.far = 5;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+scene.add(spotLight);
+scene.add(spotLight.target);
+
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+spotLightCameraHelper.visible = false;
+scene.add(spotLightCameraHelper);
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+
+pointLight.castShadow = true;
+pointLight.position.set(2, 2, 2);
+pointLight.shadow.mapSize.set(1024, 1024);
+pointLight.shadow.camera.far = 8;
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera);
+pointLightCameraHelper.visible = false;
+scene.add(pointLightCameraHelper);
+
+scene.add(pointLight);
 /**
  * Objects
  */
 // Material
 const material = new THREE.MeshStandardMaterial();
-material.metalness = 0.5;
-material.roughness = 0.4;
+material.metalness = 0.3;
+material.roughness = 0.3;
 
 // Objects
 const sphere = new THREE.Mesh(
@@ -86,7 +120,7 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 /**
- * Renderer
+ * 
  */
 const renderer = new THREE.WebGLRenderer({
   canvas,
@@ -95,7 +129,7 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 renderer.shadowMap.enabled = true;
-
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 /**
  * Animate
  */
